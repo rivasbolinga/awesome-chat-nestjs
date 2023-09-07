182c7fa53f4653c34b4bb0332145db9f2baf757b
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-09-07 13:04:23 +0200

    emit

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,16 +1,22 @@
-import { SubscribeMessage, MessageBody, ConnectedSocket, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  MessageBody,
+  ConnectedSocket,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
 @WebSocketGateway()
-
 export class EventsGateway {
-  @WebSocketServer()
+  @WebSocketServer() //Get access to server
   server: Server;
-  // @SubscribeMessage('send-message')
-  // handleEvent(
-  //   @MessageBody() data: string,
-  //   @ConnectedSocket() client: Socket,
-  // ): string {
-  //   return data;
-  // }
+  @SubscribeMessage('send-message')
+  handleEvent(
+    @MessageBody() data: string, // extract message from payload
+    @ConnectedSocket() client: Socket,
+  ): void {
+    this.server.emit
+    console.log(data)
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
